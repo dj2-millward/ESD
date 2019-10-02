7af4cbbc038184aa0cb8a0b7930f4e1307fc5768
Dan Millward <daniel2.millward@live.uwe.ac.uk>
2019-10-02 20:27:38 +0100

    Assignment
    Average of 3 numbers

--- HelloWorld_/main.cpp
+++ HelloWorld_/main.cpp
@@ -1,24 +1,33 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    // std::cout << "Welcome to my program" << std::endl;
-
     //Initialise interger variables
     int a = 0;
     int b = 0;
+    int c = 0;
+    float avg;
+
+    // std::cout << "Welcome to my program" << std::endl;
 
     // ask the user for an input
-    std::cout << "Enter a number: ";
+    std::cout << "Enter 1st number: " << std::endl;
     //user input to a variable
     std::cin >> a;
 
     //user input to a variable
-    std::cout << "Enter a 2nd number: ";
+    std::cout << "Enter a 2nd number: " << std::endl;
     //user input to a variable
     std::cin >> b;
 
+    //user input to a variable
+    std::cout << "Enter a 3rd number: " << std::endl;
+    //user input to a variable
+    std::cin >> c;
+
+    avg=(a+b+c)/3;
+
     //output the variable
-    std::cout << "Your number is " << a + b << std::endl;
+    std::cout << "The average is " << avg << std::endl;
     return 0;
+
 }
