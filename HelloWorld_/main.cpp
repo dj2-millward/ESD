6fb6617be5d5ecfa16d76808162caa5cb7076805
Dan Millward <daniel.2millward@live.uwe.ac.uk>
2019-10-08 17:06:35 +0100

    Working Guessing Game

--- HelloWorld_/main.cpp
+++ HelloWorld_/main.cpp
@@ -1,32 +1,201 @@
-#include <cstdlib>
 #include <iostream>
-#include <ctime>
+#include <time.h>
+#include <stdlib.h>
+
+
 
 int main() {
-    int user_input = 0;
-    int target_num = std::rand() % 10 + 1;
 
-    std::srand(std::time(nullptr)); //seed
+    std::srand(time(NULL));
 
-    std::cout << "Hello, Welcome to 'Guess the number' !" << std::endl;
-    std::cout << "Guess a number; " << std::endl;
+    int rand_number = rand() % 10 + 1;
+    int user_input;
+    int guessCount = 0;
+    int limit = 5;
+    bool is_running = false;
 
-    std::cin >> user_input;
-    std::cout << target_num << '\n';
+    std::cout << "Hello and welcome to THE GUESSING GAME! " << std::endl;
+    _sleep(1500);
+    while (user_input != rand_number && !is_running) {
+        if (guessCount < limit) {
+            std::cout << "Enter a number between 1 and 10: ";
+            std::cin >> user_input;
+            guessCount++;
 
-    if (user_input == target_num) {
-        std::cout <<"Correct" << std::endl;
+            if (user_input < rand_number) {
+                std::cout << "Higher" << std::endl;
+            }
+            else if (user_input > rand_number) {
+                std::cout << "Lower" << std::endl;
+            }
+            else {
+                std::cout << "Well done! The number was indeed " << rand_number << std::endl;
+            }
+
+        } else {
+            is_running = true;
+            std::cout <<"You Lose, the answer was: " << rand_number << "" << std::endl;
+        }
+
     }
-    else (user_input != target_num);{
-        std::cout <<"Incorrect" << std::endl;
-
-    }
-
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
+
+/*
+int main() {
+    std::srand(time(0));
+
+    int rand_number = rand() % 10 + 1;
+    int user_input;
+    int limit = 0;
+    bool is_running = true;
+
+
+    while (is_running = true) {
+        std::cout << "Enter Number: " << std::endl;
+        std::cin >> user_input;
+
+        if (user_input < rand_number) {
+            std::cout << "Higher" << std::endl;
+        }
+            else if (user_input > rand_number) {
+                std::cout << "Lower" << std::endl;
+        }
+            else {
+                std::cout << "Correct" << std::endl;
+        }
+        while (user_input != rand_number) {
+        }
+        return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Attempt 3
+   // int the_number;
+   // int guess;
+   // int tries;
+
+  //  the_number = rand() % 10 + 1;
+
+ //   std::cout << "Let's play a game!";
+ //   std::cout << "I will think of a number 1-100. Try to guess it.";
+//   std::cout << std::endl;
+ //   std::cin >> guess;
+
+//    for (tries = 0; tries++;)
+  //  {
+  //      if (guess == the_number)
+   //     {
+   //         std::cout << "You guessed it!";
+   //         std::cout << "And it only took you: " << tries.\n"";
+   //     }
+     //   else if (guess < the_number)
+   //     {
+    //        std::cout << "Higher";
+    //        std::cin >> tries++;
+    //    }
+
+
+    //    else if (guess > the_number)
+    //    {
+    //        std::cout << "Lower";
+    //        std::cin >> tries++;
+   //    }
+
+   //     else
+  //         std::cout << "That's not even in range!";
+   //     return 0;
+
+
+
+    // Attempt 2
+    //std::srand(time(0));
+
+    //int rand_number = rand() % 10 + 1;
+   // int user_input;
+   // int limit = 0;
+   // bool is_running = true;
+
+
+   // while (is_running = true) {
+    //    std::cout << "Enter Number: " << std::endl;
+    //    std::cin >> user_input;
+
+    //    if (user_input < rand_number) {
+    //        std::cout << "Higher" << std::endl;
+     //       ++limit;
+
+     //   else if (user_input > rand_number) {
+     //       std::cout << "Lower" << std::endl;
+      //      limit++;
+    //    }
+      //  else {
+     //       std::cout << "Correct" << std::endl;
+      //      limit != 6;
+      //     is_running = false;
+      //
+
+
+//}
+      // while (user_input != rand_number) ;
+
+
+
+
+
+
+
+
+
+
+
+    // While limit != 5
+    // else if
+
+//do {
+//std::cout << "Enter Number: " << std::endl;
+//std::cin >> user_input;
+
+//if (user_input < rand_number)
+//std::cout << "Higher" << std::endl;
+//else if (user_input > rand_number)
+//std::cout << "Lower" << std::endl;
+//else
+//std::cout << "Correct" << std::endl;
+//} while (user_input != rand_number) ;
+//return 0;
+
 
 
 
@@ -59,3 +228,4 @@ int main() {
     //output the variable
    // std::cout << "Your number is " << a + b << std::endl;
    // return 0;
+/**/
