eb483203a1c2227c587a3f67d7e6096a4375a2fa
Dan Millward <daniel.2millward@live.uwe.ac.uk>
2019-10-08 14:14:01 +0100

    Started Worksheet 1
    If Else

---
+++ HelloWorld_/cmake-build-debug/CMakeFiles/HelloWorld_.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloWorld_.dir/main.cpp.obj"
+  "HelloWorld_.pdb"
+  "HelloWorld_.exe"
+  "HelloWorld_.exe.manifest"
+  "libHelloWorld_.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HelloWorld_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- HelloWorld_/main.cpp
+++ HelloWorld_/main.cpp
@@ -1,24 +1,61 @@
+#include <cstdlib>
 #include <iostream>
+#include <ctime>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int user_input = 0;
+    int target_num = std::rand() % 10 + 1;
+
+    std::srand(std::time(nullptr)); //seed
+
+    std::cout << "Hello, Welcome to 'Guess the number' !" << std::endl;
+    std::cout << "Guess a number; " << std::endl;
+
+    std::cin >> user_input;
+    std::cout << target_num << '\n';
+
+    if (user_input == target_num) {
+        std::cout <<"Correct" << std::endl;
+    }
+    else (user_input != target_num);{
+        std::cout <<"Incorrect" << std::endl;
+
+    }
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+    // std::cout << "Hello, World!" << std::endl;
     // std::cout << "Welcome to my program" << std::endl;
 
     //Initialise interger variables
-    int a = 0;
-    int b = 0;
+    //int a = 0;
+   // int b = 0;
 
     // ask the user for an input
-    std::cout << "Enter a number: ";
+    //std::cout << "Enter a number: ";
     //user input to a variable
-    std::cin >> a;
+   // std::cin >> a;
 
     //user input to a variable
-    std::cout << "Enter a 2nd number: ";
+   // std::cout << "Enter a 2nd number: ";
     //user input to a variable
-    std::cin >> b;
+   // std::cin >> b;
 
     //output the variable
-    std::cout << "Your number is " << a + b << std::endl;
-    return 0;
-}
+   // std::cout << "Your number is " << a + b << std::endl;
+   // return 0;
